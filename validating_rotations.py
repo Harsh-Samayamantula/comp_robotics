a49618fa208bc5444590f465c4ac4637f9b6c4b8
Harsh Samayamantula <hs1018@scarletmail.rutgers.edu>
2024-09-23 16:14:40 -0400

    random rot matrices + random quaternions

--- validating_rotations.py
+++ validating_rotations.py
@@ -1,7 +1,7 @@
-import numpy
+import numpy as np
 import matplotlib
 import scipy.stats
-
+from utils import *
 
 def check_SOn(m, epsilon=0.01):
     # Orthogonality: m^tm = I
@@ -65,4 +65,97 @@ def correct_quaternion(v, epsilon=0.01):
     return v
 
 
-    
+
+
+def random_rotation_matrix(naive=True):
+    if naive:
+        # Naive solution --> Random euler angles and multiplying rotation matrices
+        alpha = np.random.uniform(0, 2*np.pi)
+        beta = np.random.uniform(0, 2*np.pi)
+        gamma = np.random.uniform(0, 2*np.pi)
+
+        # alpha, beta, gamma = random_euler_angles()
+
+
+        R_z = np.array([[np.cos(alpha), -np.sin(alpha), 0],
+                        [np.sin(alpha), np.cos(alpha), 0],
+                        [0, 0, 1]])
+        
+        R_y = np.array([[np.cos(beta), 0, np.sin(beta)],
+                        [0, 1, 0],
+                        [-np.sin(beta), 0, np.cos(beta)]])
+
+        R_x = np.array([[1, 0, 0],
+                        [0, np.cos(gamma), -np.sin(gamma)],
+                        [0, np.sin(gamma), np.cos(gamma)]])
+
+        res = R_z @ R_y @ R_x
+
+        print(check_SOn(res))
+
+        return res
+
+
+    else:
+        # Efficient method using householder matrix
+        x1 = np.random.uniform(0, 1)
+        x2 = np.random.uniform(0, 1)
+        x3 = np.random.uniform(0, 1)
+
+        theta = 2*np.pi*x1 # rotation about pole
+        phi = 2*np.pi*x2 # direction to deflect pole
+        z = x3 # pole deflection
+
+        V = np.array([np.cos(phi)*np.sqrt(z),
+                    np.sin(phi)*np.sqrt(z),
+                    np.sqrt(1-z)])
+        
+        B = np.array([[np.cos(theta), np.sin(theta), 0],
+                    [-np.sin(theta), np.cos(theta), 0],
+                    [0, 0, 1]])
+        
+        
+        M = 2 * V @ V.T - np.eye(3)
+
+        res = M @ B
+
+        print(check_SOn(res))
+
+        return res
+
+
+def random_quaternion(naive=True):
+    if naive:
+        alpha = np.random.uniform(0, 2*np.pi)  # Yaw
+        beta = np.random.uniform(0, 2*np.pi)     # Pitch
+        gamma = np.random.uniform(0, 2*np.pi)  # Roll
+
+        w = np.cos(alpha/2) * np.cos(beta/2) * np.cos(gamma/2) + np.sin(alpha/2) * np.sin(beta/2) * np.sin(gamma/2)
+        x = np.sin(alpha/2) * np.cos(beta/2) * np.cos(gamma/2) - np.cos(alpha/2) * np.sin(beta/2) * np.sin(gamma/2)
+        y = np.cos(alpha/2) * np.sin(beta/2) * np.cos(gamma/2) + np.sin(alpha/2) * np.cos(beta/2) * np.sin(gamma/2)
+        z = np.cos(alpha/2) * np.cos(beta/2) * np.sin(gamma/2) - np.sin(alpha/2) * np.sin(beta/2) * np.cos(gamma/2)
+        
+        q =  np.array([w, x, y, z])
+
+        print(check_quaternion(q))
+
+        return q
+        
+    else:
+        s = np.random.uniform()
+        sigma1 = np.sqrt(1-s)
+        sigma2 = np.sqrt(s)
+
+        theta1 = np.random.uniform(0, 2*np.pi)
+        theta2 = np.random.uniform(0, 2*np.pi)
+
+        w = np.cos(theta2)*sigma2
+        x = np.sin(theta1)*sigma1
+        y = np.cos(theta1)*sigma1
+        z = np.sin(theta2)*sigma2
+
+        q = np.array([w, x, y, z])
+
+        print(check_quaternion(q))
+
+        return q
